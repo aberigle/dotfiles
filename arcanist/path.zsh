e10f70f4f39407d7a76b57d17140a1cd5ca0a069
aberigle <a.berzosa.iglesias@gmail.com>
2015-03-30 18:05:25 +0200

    carefull if `arcanist` is not installed

--- arcanist/path.zsh
+++ arcanist/path.zsh
@@ -2,4 +2,6 @@ export ARCANIST=$HOME/arcanist/arcanist
 export PATH=$PATH:$ARCANIST/bin
 
 #arcanist bash completion
-source $ARCANIST/resources/shell/bash-completion
+if [ -z $ARCANIST/resources/shell/bash-completion ]; then
+  source $ARCANIST/resources/shell/bash-completion
+fi
