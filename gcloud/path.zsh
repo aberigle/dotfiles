97582b240488873d186ebf26b8c6782fb89cc2e0
aberigle <a.berzosa.iglesias@gmail.com>
2016-09-22 12:51:33 +0200

    update gcloud

--- gcloud/path.zsh
+++ gcloud/path.zsh
@@ -3,3 +3,6 @@
 #
 # # The next line enables bash completion for gcloud.
 # source /Users/$USER/google-cloud-sdk/completion.bash.inc
+
+export GCLOUD_HOME=~/google-cloud-sdk
+export PATH=$GCLOUD_HOME/bin:$PATH
